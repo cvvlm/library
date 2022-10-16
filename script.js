32ad50b4597aa96012cf38d9b52bc879cf1d6d00
cvvlm <michalsk7@gmail.com>
2022-10-16 21:28:39 +0200

    Whole logic
    Need to style it only, everything is done from the logic point.

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-const submit = document.querySelector('#submit')
-const tablica = document.querySelector('.tablica')
-ar = []
-function addBook(b, a, s) {
-    function BookObject(book, author, selection) {
-        this.book = book
-        this.author = author
-        this.selection = selection
+const btn = document.querySelector('#btn')
+let library = []
+
+const addBook = (ev) => {
+    ev.preventDefault()
+    let Book = {
+        title: document.querySelector('#title').value,
+        author: document.querySelector('#author').value,
+        select: document.querySelector('#select').value,
     }
-    return new BookObject(b, a, s)
-    
+    const dbtn = document.createElement('button')
+    const change = document.createElement('button')
+    dbtn.type = 'button'
+    change.type = 'button'
+    dbtn.className = 'delete'
+    change.className = 'change'
+    dbtn.textContent = "Delete"
+    change.textContent = "Change to Read!"
+    library.push(Book)
+
+    let msg = document.querySelector('#msg')
+    let pre = document.createElement('pre')
+    let title = library[library.length-1].title
+    let author = library[library.length-1].author
+    let select = library[library.length-1].select
+    if (title.length == 0 || author.length == 0) {
+        alert("Please fill all forms")
+    }else {
+        pre.textContent += '\n' + `${title} ${author} ${select}`
+        pre.appendChild(dbtn)
+        pre.appendChild(change)
+        msg.appendChild(pre)
+        dbtn.addEventListener('click', () => {
+            dbtn.parentElement.remove()
+        })
+        change.addEventListener('click', () => {
+            if (select === "Not read") {
+                select = "Read"
+                pre.textContent = '\n' + `${title} ${author} ${select}`
+                pre.appendChild(dbtn)
+                pre.appendChild(change)
+            }else if (select === "Read") {
+                select = "Not read"
+                pre.textContent = '\n' + `${title} ${author} ${select}`
+                pre.appendChild(dbtn)
+                pre.appendChild(change)
+            }
+        })
+        document.querySelector('form').reset()
+    }
 }
-function addToArr() {
-    submit.addEventListener('click', () => {
-        ar.push(addBook(document.querySelector('#book').value, document.querySelector('#author').value, document.querySelector('#select').value))
-           
-        tablica.innerHTML += `
-            <tr>
-                <td>${ar[ar.length - 1].book}</td>
-                <td>${ar[ar.length - 1].author}</td>
-                <td>${ar[ar.length - 1].selection}</td>
-            </tr>
-        `
-    })
-}
-addToArr()
+
+btn.addEventListener('click', addBook)
